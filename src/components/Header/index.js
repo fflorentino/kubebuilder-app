6d899d43044f91491f6d547eda413d4dba0cff4b
Fabio Maran Florentino <fabio.florentino@platformbuilders.io>
2020-03-18 14:23:46 -0300

    add pagination to product list

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
- <header id="main-header">KubeBuilder App</header>
+ <header id="main-header"> KubeBuilder App</header>
 );
 
 export default Header;
